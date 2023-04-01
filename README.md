ced560a08bbf9f3ef351f564e32f5cc8d700bc31
Ujjal Dutta <ujjal@iosandweb.net>
2023-04-01 14:32:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Horizontal Loop
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/GreenSock/pen/PoJqoEQ](https://codepen.io/GreenSock/pen/PoJqoEQ).
+
---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db="school";
+	/*Create connection*/
+	$conn = mysqli_connect($servername, $username, $password,$db);
+?>
+<?php $servername ="localhost";$username="root";$password ="";$db ="school";/*create  connection*/$con =mysqli
