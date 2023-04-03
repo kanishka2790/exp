6afce9aa76e09362c2a23fa1bb7aa9ad764a5833
kanishka2790 <129576901+kanishka2790@users.noreply.github.com>
2023-04-03 11:57:09 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,16 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db="school";
-	/*Create connection*/
-	$conn = mysqli_connect($servername, $username, $password,$db);
-?>
-<?php $servername ="localhost";$username="root";$password ="";$db ="school";/*create  connection*/$con =mysqli
+<?php
+
+	$servername = "localhost";
+
+	$username = "root";
+
+	$password = "";
+
+	$db="school";
+
+	/*Create connection*/
+
+	$conn = mysqli_connect($servername, $username, $password,$db);
+
+?>
+
